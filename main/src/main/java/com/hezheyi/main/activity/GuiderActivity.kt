1445de8460346c33f789d8c74f1fc8671ea5237f
邓安邦 <wjsdab@163.com>
2021-09-25 21:19:45 +0800

    修改模块

--- main/src/main/java/com/hezheyi/main/activity/GuiderActivity.kt
+++ main/src/main/java/com/hezheyi/main/activity/GuiderActivity.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
 import com.abxh.media.audio.R
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
 
 import com.hezeyi.common.base.BaseActivity
 import com.hezheyi.main.viewmodel.UserModel
@@ -21,18 +23,41 @@ import com.hezheyi.main.viewmodel.UserModel
  * Created by dab on 2021/9/25 17:24
  */
 class GuiderActivity : BaseActivity<UserModel>() {
+    @ExperimentalPagerApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        val items = mutableListOf(
+            R.mipmap.guider_1,
+            (R.mipmap.guider_2),
+            (R.mipmap.guider_3),
+            (R.mipmap.guider_4)
+        )
         setContent {
-            pageContent()
+            val state = rememberPagerState(pageCount = items.size)
+            HorizontalPager(state = state) {
+                PageContent(items[it])
+            }
+
         }
     }
 }
 
+@Preview
 @Composable
-fun pageContent() {
-    Column(modifier = Modifier.padding(10.dp).height(420.dp).background(color = Color.White)) {
-        Image(painter = painterResource(id = R.mipmap.guider_1), "")
+fun PageContent(id: Int) {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .background(color = Color.White)
+    ) {
+        Image(
+            painter = painterResource(id = id),
+            "",
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentScale = ContentScale.FillWidth
+        )
     }
 }
--- main/src/main/java/com/hezheyi/main/activity/SplashActivity.java
+++ main/src/main/java/com/hezheyi/main/activity/SplashActivity.java
@@ -3,6 +3,7 @@ package com.hezheyi.main.activity;
 import android.content.Intent;
 
 import com.abxh.core.MmkvHelper;
+import com.abxh.jetpack.IViewModel;
 import com.hezeyi.common.base.BaseActivity;
 
 /**
@@ -12,15 +13,13 @@ import com.hezeyi.common.base.BaseActivity;
  * @time 2017/2/6 9:00
  */
 
-public class SplashActivity extends BaseActivity {
-
-
+public class SplashActivity extends BaseActivity<IViewModel> {
     @Override
     public void initView() {
         super.initView();
         // TODO: 2021/9/25   //上次登录以后,超过了3天,清空密码,重新登录
         Boolean isGuiderActivity = MmkvHelper.getInstance().getBoolean("isGuiderActivity", false);
-        if (!!isGuiderActivity) {
+        if (!isGuiderActivity) {
             startActivity(new Intent(SplashActivity.this, GuiderActivity.class));
             finish();
         }else {
